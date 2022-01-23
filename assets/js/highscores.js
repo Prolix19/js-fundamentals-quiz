3c468bc9c5006276c21e16d99b5b16f87ffb81be
Prolix <51418825+Prolix19@users.noreply.github.com>
2022-01-23 17:35:22 -0600

    Add files, initial HTML skeleton, pseudocode comments

---
+++ assets/js/highscores.js
@@ -0,0 +1,4 @@
+// Add code to:
+// - Parse local storage and display the objects in a high scores table
+// - Go back button to reload index.html
+// - Clear local storage when a "Clear high scores" button is clicked
---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Add code to:
+// Hold quiz questions and answers
+// Start a countdown timer from 75 seconds when "Start Quiz" button is clicked
+// Parse click events on each answer, evaluating correct/incorrect, applying time penalty if incorrect, loading next question or quiz end as appropriate, providing feedback on last response.
+// Handle form to submit final score and initials
+// Throw high score data into local storage
