15f57cc3c61af0806d792d6f0677715cb556d2f7
Prolix <51418825+Prolix19@users.noreply.github.com>
2022-01-24 20:26:35 -0600

    More attepts at the JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,15 +27,19 @@ var quizArray = [
     }
 ];
 
+// Declare & initialize some global variables for tracking the time remaining, score, and number of questions
+var quizTimer = 75;
+var score = 0;
+var currentQuestion = 0;
+var last = 4;
+
+// Bringing in some elements from the HTML
 var headerElement = document.querySelector("#head");
 var quizHTML = document.querySelector("#quizarea");
-
-var quizTimer = 75;
-
 var startQuizButton = document.querySelector("#startquizbutton");
-
 var timerElement = document.querySelector("#timer");
 
+// Build out buttons for the four possible responses to each question
 var answerButton1 = document.createElement("button");
 answerButton1.setAttribute("type", "button");
 answerButton1.setAttribute("class", "btn");
@@ -60,39 +64,83 @@ answerButton4.setAttribute("class", "btn");
 answerButton4.setAttribute("id", "btn3");
 answerButton1.setAttribute("data-num", "4");
 
+// Populate the initial HTML -- heading, description of the quiz, and a button to start it
+var pageH1 = document.createElement("h1");
+pageH1.setAttribute("id", "h1");
+pageH1.textContent = "Coding Quiz Challenge";
+quizHTML.appendChild(pageH1);
+
+var pageP = document.createElement("p");
+pageP.setAttribute("id", "text");
+pageP.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+quizHTML.appendChild(pageP);
+
+var startQuizButton = document.createElement("button");
+startQuizButton.setAttribute("type", "button");
+startQuizButton.setAttribute("class", "btn");
+startQuizButton.setAttribute("id", "start");
+startQuizButton.textContent = "Start Quiz";
+quizHTML.appendChild(startQuizButton);
+
+// This setInterval function keeps track of the time remaining and ends the game as needed.
 function countdown() {
     var timeInterval = setInterval(function () {
-      if (quizTimer > 0) {
         timerElement.textContent = "Time: " + quizTimer;
         quizTimer--;
-      } else {
-        timerElement.textContent = "Time: 0";
-        clearInterval(timeInterval);
-        // Call func to end game
-        // return false;
-      }
+        if (quizTimer <= 0) {
+            timerElement.textContent = "Time: 0";
+            clearInterval(timeInterval);
+            endQuiz();
+        } else if (currentQuestion === last) {
+            clearInterval(timeInterval);
+        }
     }, 1000);
-  }
+}
 
+function makeAnswerBtns() {
+    var qtemp = quizArray[currentQuestion];
+    var answers = document.createElement("div");
+    answers.setAttribute("id", "answers");
+    quizHTML.appendChild(answers);
+    var answersDiv = document.querySelector("#answers");
+    answersDiv.appendChild(answerButton1);
+    answersDiv.appendChild(answerButton2);
+    answersDiv.appendChild(answerButton3);
+    answersDiv.appendChild(answerButton4);
+    var textSpan;
+    for (var i = 0; i < qtemp.responses.length; i++) {
+        textSpan = document.createElement("span");
+        textSpan.setAttribute("data-answer", "option" + i);
+        textSpan.setAttribute("id", "option" + i);
+        document.querySelector(("#btn" + i)).appendChild(textSpan);
+    };
+};
+
+function showQuestion() {
+    var qtemp = quizArray[currentQuestion];
+    pageH1.textContent = qtemp.question;
+    for (var i = 0; i < qtemp.responses.length; i++) {
+        document.querySelector("#option" + i).textContent = qtem.responses[i];
+    }
+};
 
 var startQuiz = function(event) {
+    // Preempt default event handling and hide a couple elements
     event.preventDefault;
+    document.querySelector("#text").style.display = "none";
+    document.querySelector("#start").style.display = "none";
+
+    // Start the timer, make answer buttons, and show the first question
     countdown();
-    var currentQuestion = 0;
-    quizHTML.innerHTML = "<h1>" + quizArray[currentQuestion].question + "</h1>";
-    quizHTML.appendChild()
-    function showAnswers() {
-        var question = quizArray[currentQuestion];
-        var answers = createEle
-    }
+    makeAnswerBtns();
+    showQuestion();
 
-
-    function createElement(element, type, value, text) {
-        var tmp = document.createElement(element);
-        tmp.setAttribute(type, value);
-        tmp.textContent = text;
-        return tmp;
+    // Add event listeners to the answer buttons
+    var answerList = document.querySelectorAll(".answers");
+    for (var i = 0; i < answerList.length; i++) {
+        answerList[i].addEventListener('click', checkAnswer)
     };
 }
 
 startQuizButton.addEventListener("click", startQuiz);
+
